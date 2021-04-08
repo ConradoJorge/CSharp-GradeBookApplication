80b6c4c95232e9e34fabf321db379af21705ec72
Conrado Jorge <67123333+ConradoJorge@users.noreply.github.com>
2021-04-07 22:21:32 -0400

    Added ranked grading to RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,7 @@
-﻿namespace GradeBook.GradeBooks
+﻿using System;
+using System.Linq;
+
+namespace GradeBook.GradeBooks
 {
     public class RankedGradeBook : BaseGradeBook
     {
@@ -6,5 +9,27 @@
         {
             Type = Enums.GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("You must have at least 5 students to do ranked grading.");
+            }
+
+            var section = (int)Math.Ceiling(Students.Count * 0.2);
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+            if (averageGrade >= grades[section - 1])
+                return 'A';
+            if (averageGrade >= grades[(section * 2)- 1])
+                return 'B';
+            if (averageGrade >= grades[(section * 3) - 1])
+                return 'C';
+            if (averageGrade >= grades[(section * 4) - 1])
+                return 'D';
+            return 'F';
+        }
+
     }
 }
